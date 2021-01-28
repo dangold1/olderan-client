e8f1b6206c40e5eb49cfe99b4895a1ee4dc1d689
dangold1 <dangold308@gmail.com>
2021-01-28 19:53:05 +0200

    update env

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -1,5 +1,6 @@
-export const IMAGES_API = "https://jsonplaceholder.typicode.com/photos";
-export const USERS_API = "http://localhost:3000/api/admin/users";
-export const REGISTER_USER_API = "http://localhost:3000/api/users/register";
-export const SIGNIN_USER_API = "http://localhost:3000/api/users/signin";
-export const STORAGE_KEY = "OLDERAN_USER";
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+export const IMAGES_API = `https://jsonplaceholder.typicode.com/photos`;
+export const USERS_API = `${SERVER_URL}/api/admin/users`;
+export const REGISTER_USER_API = `${SERVER_URL}/api/users/register`;
+export const SIGNIN_USER_API = `${SERVER_URL}/api/users/signin`;
+export const STORAGE_KEY = `OLDERAN_USER`; 
